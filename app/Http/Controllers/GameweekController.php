6390b6fd81d6df01641709bb839977edc4fbf845
Andy <amcdonald1307@protonmail.com>
2023-08-17 18:16:24 +0100

    Removed redundant comments

--- app/Http/Controllers/GameweekController.php
+++ app/Http/Controllers/GameweekController.php
@@ -11,19 +11,12 @@ class GameweekController extends Controller
 {
     public static function store($friday, $thursday)
     {
-        // $attributes = request()->validate([
-        //     'start_date' => ['required', 'date', Rule::unique('gameweek', 'start_date')],
-        //     'end_date' => ['required', 'date', Rule::unique('gameweek', 'end_date')]
-        // ]);
-
         $attributes = array(
             'start_date' => $friday,
             'end_date' => $thursday
         );
 
         Gameweek::create($attributes);
-
-        // return redirect('/');
     }
 
     public static function checkGameweekExists($startdate, $enddate)
